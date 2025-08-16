91d0f9edce3a9d49930b0aa9ea5023d93615ee19
Thomas Damon <nomadmot@mac.com>
2025-08-16 14:39:32 -0600

    configure and initialize loggers for streamlit and sqlalchemy

--- Portfolio-Dashboard/app.py
+++ Portfolio-Dashboard/app.py
@@ -1,4 +1,15 @@
 import streamlit as st
+import streamlit.logger
+import config
+
+# Initialize logging
+logger = streamlit.logger.get_logger(st.__name__)
+logger.setLevel(config.LOGLEVEL_STREAMLIT)
+# mark entry into the module
+logger.debug("Starting Portfolio Dashboard application")
+# set the sqlalchemy logging level
+sqlalchemy_logger = streamlit.logger.get_logger("sqlalchemy.engine")
+sqlalchemy_logger.setLevel(config.LOGLEVEL_SQLALCHEMY)
 
 # use the stock bull icon
 st.logo(image="images/Stock-Bull.png",)
--- Portfolio-Dashboard/config.py
+++ Portfolio-Dashboard/config.py
@@ -1,7 +1,11 @@
 """
 Configuration settings for the application.
 """
-from sqlalchemy import create_engine
+# logging configuration
+LOGLEVEL_STREAMLIT = "debug"
+LOGLEVEL_SQLALCHEMY = "error"
 
+# database configuration
+from sqlalchemy import create_engine
 DATABASE_URI = "sqlite://///Users/nomadmot/Library/CloudStorage/Dropbox/Apps/Investing/DATA/portfolio-test.db"
-DB_ENGINE = create_engine(DATABASE_URI, echo=True)
+DB_ENGINE = create_engine(DATABASE_URI, echo=False)
