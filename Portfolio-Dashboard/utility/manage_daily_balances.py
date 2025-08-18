76097b1b4da0a06f97f77d565305047f67e72d96
Thomas Damon <nomadmot@mac.com>
2025-08-18 13:16:39 -0600

    adapt to use in this context: use logger, raise exceptions, etc.

--- Portfolio-Dashboard/utility/manage_daily_balances.py
+++ Portfolio-Dashboard/utility/manage_daily_balances.py
@@ -4,16 +4,24 @@ Routines for accessing and manipulating the portfolio database.
 # Import necessary libraries
 from datetime import date, datetime as dt
 from typing import Optional
+import streamlit as st
+import streamlit.logger
 from sqlalchemy.orm import Session
 
 # Import local modules
 from models.portfolio import DailyBalance, Account
 import config
 
+# Initialize the logger
+logger = streamlit.logger.get_logger(st.__name__)
+# mark entry into the module
+logger.debug("Entering manage_daily_balances module")
+
+
 # function to update the portfolio balance for a specified account and day
 def update_daily_balance(
-    balance_amount: float,
     account_id: int,
+    balance_amount: float,
     balance_date: date
 ) -> None:
     """
@@ -23,24 +31,28 @@ def update_daily_balance(
     :param account: The account number to update.
     :param date: The date for which the balance is being updated.
     """
-    
+
+    # mark entry into the function
+    logger.debug(
+        "entering update_daily_balance, account_id: %s balance_date: %s balance_amount %s",
+        account_id,
+        balance_date,
+        balance_amount
+        )
+
     # check if the input balance date and amount are valid.
     if balance_date is None or balance_amount is None:
-        st.error("Please select a date and enter a balance amount.")
-        return False
+        raise ValueError("Please select a date and enter a balance amount.")
     if balance_amount < 0:
-        st.error("Balance cannot be negative.")
-        return False
+        raise ValueError("Balance cannot be negative.")
     
-    # ensure the account exists and get the account name
-    with Session(config.DB_ENGINE) as session:
-        account: Optional[Account] = session.get(Account, account_id)
-        if not account:
-            raise ValueError(f"Account with ID {account_id} does not exist.")
-        account_name: str = account.name
-        session.close()
-
-    print(f"Adding daily balance for {account_name} on {balance_date} with balance {balance_amount}")
+    # # ensure the account exists and get the account name
+    # with Session(config.DB_ENGINE) as session:
+    #     account: Optional[Account] = session.get(Account, account_id)
+    #     if not account:
+    #         raise ValueError(f"Account with ID {account_id} does not exist.")
+    #     account_name: str = account.name
+    #     session.close()
     
     # create a new DailyBalance object
     daily_balance: DailyBalance = DailyBalance(
@@ -48,13 +60,18 @@ def update_daily_balance(
         account_id=account_id,
         date=balance_date
     )
-    print(f"Daily balance object created: {daily_balance}")
+    logger.debug(
+        "Daily balance object created: %s",
+        daily_balance
+        )
 
-    # # save the daily balance to the database
-    # with Session(config.DB_ENGINE) as session:
-    #     session.add(daily_balance)
-    #     session.commit()
-    #     print("Daily balance updated successfully.")
+    # save the daily balance to the database
+    with Session(config.DB_ENGINE) as session:
+        session.add(daily_balance)
+        session.commit()
+        
+    logger.info("Daily balance updated successfully")
+
 
 # function to delete the daily balance for a specified account and day
 def delete_daily_balance(
@@ -67,12 +84,22 @@ def delete_daily_balance(
     :param account_id: The ID of the account to delete the balance for.
     :param date: The date for which the balance is being deleted.
     """
+
+    # mark entry into the function
+    logger.debug(
+        "entering delete_daily_balance, account_id: %s balance_date: %s",
+        account_id,
+        balance_date,
+        )
+    
     # ensure the account exists and get the account name
     with Session(config.DB_ENGINE) as session:
         account: Optional[Account] = session.get(Account, account_id)
         if not account:
-            raise ValueError(f"Account with ID {account_id} does not exist.")
-        account_name: str = account.name
+            raise ValueError(
+                f"Account with ID {account_id} does not exist."
+                )
+        #account_name: str = account.name
         session.close()
 
     with Session(config.DB_ENGINE) as session:
@@ -81,9 +108,15 @@ def delete_daily_balance(
             DailyBalance.date == balance_date
         ).first()
         if not daily_balance:
-            raise ValueError(f"No daily balance found for account {account_name} on {balance_date}")
+            raise ValueError(
+                f"No daily balance found for account {account_id} on {balance_date}"
+                )
         session.delete(daily_balance)
         session.commit()
-        print(f"Daily balance for account {account_name} on {balance_date} deleted successfully.")
+        logger.info(
+            "Daily balance for account %s on %s deleted successfully.",
+            account_id,
+            balance_date
+            )
 
 #update_daily_balance(balance_amount=1000.0, account_id=1, balance_date=date.today())
