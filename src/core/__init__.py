06997f6c7580edc5f83e70418be6d06c4132c719
Thomas Damon <nomadmot@mac.com>
2025-09-07 09:48:53 -0600

    export class Periods at the module level

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -3,16 +3,20 @@ collect and expose data management and querying functions
 for the data module
 """
 
+from core.periods import Periods
+
 from core.manage_portfolio_balances import (
     update_daily_balance,
     delete_daily_balance
 )
+
 from core.query_portfolio import (
     get_account,
-    get_balance_history
+    get_balance_history,
 )
 
 __all__ = [
+    "Periods",
     "update_daily_balance",
     "delete_daily_balance",
     "get_account",
