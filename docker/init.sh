37de01e1e115c0918ac08648d9f66f8dcba92c3a
Thomas Damon <nomadmot@mac.com>
2025-09-08 16:05:31 -0600

    provide better feedback

--- docker/init.sh
+++ docker/init.sh
@@ -1,16 +1,18 @@
 #! /bin/zsh
 # This script is used to run the container for the first time
+IMAGE_NAME=portfolio-dashboard:latest
+CONTAINER_NAME=portfolio-dashboard
+echo "Initializing image $IMAGE_NAME in container $CONTAINER_NAME"
 
 # Mount the investor workspace to the container
 MOUNT_DIR=/var/investorlab
 WORKSPACE_DIR=/Users/nomadmot/Library/CloudStorage/Dropbox/Apps/Investing
-
 echo "Mounting $WORKSPACE_DIR to $MOUNT_DIR"
 
 # Start the new container
 echo "Starting container"
 podman run -it --detach --replace \
-    --name portfolio-dashboard \
+    --name $CONTAINER_NAME \
     -v $WORKSPACE_DIR:$MOUNT_DIR \
     -p 8080:8080 \
-    portfolio-dashboard:latest
+    $IMAGE_NAME
