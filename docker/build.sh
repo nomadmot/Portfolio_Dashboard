d47c88d7927738748c062e6d0e5006e00444205d
Thomas Damon <nomadmot@mac.com>
2025-09-08 14:11:02 -0600

    add push to DockerHub after build is complete, provide better feedback during build

--- docker/build.sh
+++ docker/build.sh
@@ -1,15 +1,23 @@
 #! /bin/zsh
 # This script builds the container image for the project.
+IMAGE_NAME=portfolio-dashboard
+echo "Building the container image: $IMAGE_NAME"
 
 # Remove any existing image with the same name
-podman rm -f portfolio-dashboard:latest
-podman rmi -f portfolio-dashboard:latest
+podman rmi -f $IMAGE_NAME
 
 # Change directory so build context is at project root
 cd ../
 
 # Build the image using the Dockerfile in the current directory
-podman build -f docker/Dockerfile -t portfolio-dashboard:latest .
+podman build -f docker/Dockerfile -t $IMAGE_NAME  --iidfile image_id .
+IMAGE_ID=`cat image_id`
+echo "Successfully built image with ID: $IMAGE_ID"
 
 # initialize the container
 docker/init.sh
+
+# push to DockerHub
+echo "Pushing image $IMAGE_ID to DockerHub as $IMAGE_NAME"
+podman login docker.io
+podman push $IMAGE_ID tdamon/nomadmot:$IMAGE_NAME
