9c05f33baf95925c100d54f6c400f61d39e8e3e6
Thomas Damon <nomadmot@mac.com>
2025-10-29 15:13:07 -0700

    create a layout container for the input fields

--- src/pages/detail_performance.py
+++ src/pages/detail_performance.py
@@ -25,30 +25,49 @@ st.set_page_config(layout="wide")
 # page subheader
 st.subheader("Detail Performance Analysis")
 
+# set page content layout
+with st.container(width="stretch"):
+    # we'll use only the first column for inputs
+    layout_inputs = st.columns([0.3, 0.7])[0]
+    with layout_inputs:
+        # container for input elements
+        with st.container(border=True):
+            #begin and end dates in the first row
+            with st.container(horizontal=True):
+                layout_begin_date = st.empty()
+                layout_end_date = st.empty()
+            # include options checkbox and symbol multiselect in the second row
+            with st.container(horizontal=True):
+                layout_include_options = st.empty()
+                layout_select_symbols = st.empty()
+
 # collect user inputs
 # begin and end dates for the performance analysis
-col1, col2 = st.columns(2)
-with col1:
+with layout_begin_date:
     selected_begin_date = st.date_input(
         "Begin Date:",
         value=date(date.today().year, 1, 1),
         label_visibility="collapsed",
     )
-with col2:
+with layout_end_date:
     selected_end_date = st.date_input(
         "End Date:",
         value=date.today(),
         label_visibility="collapsed",
     )
 
-# option to include options in the symbol list
-include_options = st.checkbox("Include Options")
-selected_symbols = st.multiselect(
-    "Select Symbol(s):",
-    label_visibility="collapsed",
-    placeholder="Select Symbol(s)",
-    options=get_security_symbols(include_options=include_options),
-    )
+with layout_include_options:
+    # option to include options in the symbol list
+    include_options = st.checkbox("Include Options")
+
+with layout_select_symbols:
+    # multiselect symbols for perormance analysis
+    selected_symbols = st.multiselect(
+        "Select Symbol(s):",
+        label_visibility="collapsed",
+        placeholder="Select Symbol(s)",
+        options=get_security_symbols(include_options=include_options),
+        )
 
 if selected_symbols:
     selected_trades = filter_trades(
