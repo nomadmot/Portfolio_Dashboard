4d16bbb7f9f3f118294c171b003157178336cf13
Thomas Damon <nomadmot@mac.com>
2025-11-28 15:11:24 -0700

    in progress

--- src/pages/detail_performance.py
+++ src/pages/detail_performance.py
@@ -79,5 +79,12 @@ if selected_symbols:
         selected_end_date,
     )
 
-    st.table(selected_trades)
+    st.dataframe(selected_trades,
+                 hide_index=True,
+                 use_container_width=False,
+                 column_config={
+                     "Price": st.column_config.NumberColumn(format="dollar"),
+                     "Amount": st.column_config.NumberColumn(format="dollar")
+                    }
+                 )
     st.subheader(f"Total Gain/Loss: ${selected_trades['Amount'].sum():,.2f}")
