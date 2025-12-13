4f33513c5efcb46389fec2f3226f41b0ecdbb24f
Thomas Damon <nomadmot@mac.com>
2025-12-13 16:04:22 -0700

    configure database and logging with environment variables

--- src/app.py
+++ src/app.py
@@ -2,20 +2,23 @@
 Main application file for the Portfolio Dashboard using Streamlit
 '''
 #standard library imports
+import logging
+
+#import 3rd-party libraries
 import streamlit as st
-import streamlit.logger
 
 #local application imports
 import config
 from utility import StatusMessageComponent
 
 # Initialize logging
-logger = streamlit.logger.get_logger(st.__name__)
-logger.setLevel(config.LOGLEVEL_STREAMLIT)
+#logger = logging.getLogger(st.__name__)
+logger = config.LOGGER
+#logger.setLevel(config.LOGLEVEL_STREAMLIT)
 # mark entry into the module
-logger.info("Starting Portfolio Dashboard application")
+logger.debug("Starting Portfolio Dashboard application")
 # set the sqlalchemy logging level
-sqlalchemy_logger = streamlit.logger.get_logger("sqlalchemy.engine")
+sqlalchemy_logger = logging.getLogger("sqlalchemy.engine")
 sqlalchemy_logger.setLevel(config.LOGLEVEL_SQLALCHEMY)
 
 # use the stock bull icon
