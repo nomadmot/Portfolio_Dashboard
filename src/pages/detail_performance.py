780813d9269d041ecbf1787c776da113b8cc248b
Thomas Damon <nomadmot@mac.com>
2025-12-15 15:34:18 -0700

    ffixt lint: line too long

--- src/pages/detail_performance.py
+++ src/pages/detail_performance.py
@@ -118,7 +118,10 @@ if selected_symbols:
         current_holdings = trades['Holding'].iloc[-1]
         current_price = get_basic_quote(trades['Symbol'].iloc[-1]).get('currentPrice', 0)
         current_value = current_price * current_holdings
-    st.subheader(f"Currently holding {current_holdings:.0f} shares @ ${current_price:,.2f} per share")
+    st.subheader(
+        (f"Currently holding {current_holdings:.0f} "),
+        ("shares @ ${current_price:,.2f} per share")
+    )
     st.subheader(f"Current Market Value: {current_value:,.2f}")
     total_gain_loss = current_value + selected_trades['Amount'].sum()
     st.subheader(f"Total Gain/Loss: ${total_gain_loss:,.2f}")
