df545b25405f2905c70a400db43b13b989a93869
Thomas Damon <nomadmot@mac.com>
2025-12-23 20:06:58 -0700

    group selected trades by symbol and process each group independently

--- src/pages/detail_performance.py
+++ src/pages/detail_performance.py
@@ -16,8 +16,8 @@ from core import (get_security_symbols,
                   Periods
                   )
 
-# function to analyze trades
-def analyze_trades(trades_df: pd.DataFrame) -> pd.DataFrame:
+# utility function to analyse trades for a single symbol
+def _analyze_trades(trades_df: pd.DataFrame) -> pd.DataFrame:
     """
     Analyze the current status of a trade using the trade data from the input Dataframe
 
@@ -28,24 +28,20 @@ def analyze_trades(trades_df: pd.DataFrame) -> pd.DataFrame:
         DataFrame with additional columns for analysis
     """
     # mark entry in log
-    LOGGER.debug("in analyze_trades: %s rows to analyze", trades_df.shape[0])
+    LOGGER.debug("in _analyze_trades: %s rows to analyze", trades_df.shape)
 
     # initialize variables
     total_shares = 0.0
     total_cost = 0.0
     market_value = 0.0
 
-    # add columns for analysis
-    trades_df.insert(6, "Cost", 0.0)
-    trades_df.insert(7, "Realized P/L", 0.0)
 
     # loop through each row to compute analysis metrics
-    for index in range(trades_df.shape[0]):
-        row = trades_df.iloc[index]
-        LOGGER.debug("analyzing row %s: %s", index, row.to_dict())
+    for key, row in trades_df.iterrows():
+        LOGGER.debug("analyzing row %s", row)
         # calculate the current quantity for each trade
         total_shares = total_shares + row["Quantity"]
-        trades_df.at[index, "Holding"] = total_shares
+        trades_df.loc[key, "Holding"] = total_shares
         if total_shares == 0:
             # calculate the total realized profit/loss
             realized_pl = row["Amount"] - total_cost
@@ -60,11 +56,78 @@ def analyze_trades(trades_df: pd.DataFrame) -> pd.DataFrame:
             market_value = total_shares * row["Price"]
             # update the total cost
             total_cost = total_cost - row["Amount"]
-        trades_df.at[index, "Cost"] = total_cost
-        trades_df.at[index, "Realized P/L"] = realized_pl
-        trades_df.at[index, "Market Value"] = market_value
+        trades_df.loc[key, "Cost"] = total_cost
+        trades_df.loc[key, "Realized P/L"] = realized_pl
+        trades_df.loc[key, "Market Value"] = market_value
 
     return trades_df
+
+
+# function to analyze trades
+def analyze_trades(trades_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Analyze the current status of a trade using the trade data from the input Dataframe
+
+    Arguments:
+        trades_df -- input DataFrame containing trade data to be analyzed
+
+    Returns:
+        DataFrame with additional columns for analysis
+    """
+    # mark entry in log
+    LOGGER.debug("in analyze_trades: %s rows to analyze", trades_df.shape[0])
+
+    # add columns for analysis
+    trades_df.insert(6, "Cost", 0.0)
+    trades_df.insert(7, "Realized P/L", 0.0)
+    trades_df.insert(8, "Holding", 0.0)
+    trades_df.insert(8, "Market Value", 0.0)
+
+    #### EXPERIMENT! ###
+    #indexed = trades_df.set_index("Symbol")
+    indexed = trades_df
+    analyzed = list()
+    grouped = indexed.groupby("Symbol")
+    for s in grouped:
+        symbol = s[0]
+        analyzed.append(_analyze_trades(grouped.get_group(symbol)))
+    stacked = pd.concat(analyzed)
+
+    # # initialize variables
+    # total_shares = 0.0
+    # total_cost = 0.0
+    # market_value = 0.0
+
+    # # add columns for analysis
+    # trades_df.insert(6, "Cost", 0.0)
+    # trades_df.insert(7, "Realized P/L", 0.0)
+
+    # # loop through each row to compute analysis metrics
+    # for index in range(trades_df.shape[0]):
+    #     row = trades_df.iloc[index]
+    #     LOGGER.debug("analyzing row %s: %s", index, row.to_dict())
+    #     # calculate the current quantity for each trade
+    #     total_shares = total_shares + row["Quantity"]
+    #     trades_df.at[index, "Holding"] = total_shares
+    #     if total_shares == 0:
+    #         # calculate the total realized profit/loss
+    #         realized_pl = row["Amount"] - total_cost
+    #         # If current holdings equal 0 the current market value and cost is also 0
+    #         market_value = 0.0
+    #         total_cost = 0.0
+    #     else:
+    #         # realized profit/loss is 0 if holdings are not zero
+    #         realized_pl = 0.0
+    #         # calculate the current market value
+    #         # sign of market value is opposite of trade amount
+    #         market_value = total_shares * row["Price"]
+    #         # update the total cost
+    #         total_cost = total_cost - row["Amount"]
+    #     trades_df.at[index, "Cost"] = total_cost
+    #     trades_df.at[index, "Realized P/L"] = realized_pl
+    #     trades_df.at[index, "Market Value"] = market_value
+
+    return stacked
 
 
 # configure the page layout
@@ -123,7 +186,7 @@ if selected_symbols:
         ascending=True,
     )
 
-    trades = analyze_trades(selected_trades.copy())
+    trades = analyze_trades(selected_trades)
 
     st.dataframe(trades,
                  hide_index=True,
