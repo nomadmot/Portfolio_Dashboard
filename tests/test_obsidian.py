c3db627b64b5a85a7de1729c6432b2b134817fe1
Thomas Damon <nomadmot@mac.com>
2026-01-04 11:57:15 -0700

    added test for new obsidian file creation

--- tests/test_obsidian.py
+++ tests/test_obsidian.py
@@ -3,16 +3,57 @@ exercise functions from core.obsidian module
 '''
 # import standard libraries
 import sys
+from datetime import datetime as dt
 
 # adjust path for app imports from src directory
 sys.path.append("src")
 
 # import application modules
+#pylint: disable=wrong-import-position
+from config import LOGGER
 from core.obsidian import open_obsidian_file, new_obsidian_file, search_obsidian_notes
 
+LOGGER.info("Starting Obsidian module test script at %s", dt.now().isoformat())
 # constants
 VAULT_NAME = "Stock Journal"
-FILE_NAME = "2025-11-17 14-32-44"
+FILE_NAME = "TEST"
+CONTENT = """---
+symbol: GDX
+phase: TEST
+entered: "{{date}} {{time}}"
+---
+
+***Symbol:***  `INPUT[text:symbol]`
+
+***Phase:***  `INPUT[inlineSelect(
+	option(Pre-Trade),
+	option(Post-Trade),
+	option(Note)
+	):phase]`
+
+***Entered:***  `VIEW[{entered}]`
+***Earnings Date:***
+
+### Chart:
+
+
+### Strategy:
+
+
+### Fundamental Analysis:
+
+
+### Technical Analysis:
+
+
+### Psychology:
+
+
+### Notes:
+"""
+
+# create a new file in Obsidian
+new_obsidian_file(VAULT_NAME, FILE_NAME, CONTENT)
 
 # open an existing file in Obsidian
 open_obsidian_file(VAULT_NAME, FILE_NAME)
