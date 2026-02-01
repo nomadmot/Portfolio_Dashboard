031ff79947f48e9a10df8be36550afbb42c72810
Thomas Damon <nomadmot@mac.com>
2026-02-01 14:27:15 -0700

    WIP: new function to return start and end dates for a selected period

--- src/core/periods.py
+++ src/core/periods.py
@@ -1,11 +1,15 @@
 """
-Enumeration class for time period selections.
+Provide start and end period selections
 """
+
+# standard library imports
+import logging
+from datetime import date
 from enum import Enum
 
 class Periods(Enum):
     """
-    Enumeration for time period selections
+    Enumeration class for time period selections.
     """
     D30 = "30 Days"
     D50 = "50 Days"
@@ -13,6 +17,7 @@ class Periods(Enum):
     YTD = "YTD"
     YR1 = "1 Year"
     ALL = "All"
+    CUS = "Custom"
 
     @classmethod
     def get_periods(cls):
@@ -30,3 +35,19 @@ class Periods(Enum):
             label for the period selection dropdown
         """
         return item.value
+
+
+def get_period_dates(
+            period: Periods,
+            from_date: date|None = None,
+            to_date: date|None = None) -> tuple[date, date]:
+    """
+    provide start and end dates for the given period selection
+
+    Args:
+        period (Periods): selected period
+        from_date (date, optional): custom start date for 'Custom' period. Defaults to None.
+
+    Returns:
+        tuple(date, date): start and end dates for the selected period
+    """
