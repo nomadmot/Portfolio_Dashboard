d1f65faff4a47cb46b7256293e32b31d4cccc907
Thomas Damon <nomadmot@mac.com>
2026-02-02 14:00:23 -0700

    add market_calendar functions to utility module exports

--- src/utility/__init__.py
+++ src/utility/__init__.py
@@ -2,10 +2,16 @@
 Initialize the utility module
 """
 # Import all utility module components for easy access
+from utility.market_calendar import (
+    get_closed_count,
+    market_is_open,
+)
 from utility.status_message_component import StatusMessageComponent
 from utility.autoupdate_multiselect_component import aumc_get_instance
 
 __all__ = [
+    "get_closed_count",
+    "market_is_open",
     "StatusMessageComponent",
     "aumc_get_instance",
     ]
