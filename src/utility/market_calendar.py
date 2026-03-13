f58a59e969ff6ed2c3a7a1d744513a056b564167
Thomas Damon <nomadmot@mac.com>
2026-03-12 20:14:05 -0700

    use DuckDB file interface instead of database to avoid locked database errors

--- src/utility/market_calendar.py
+++ src/utility/market_calendar.py
@@ -5,7 +5,7 @@ functions to query market holidays
 from datetime import date
 
 # third-party imports
-from duckdb import connect
+from duckdb import sql
 
 # local application imports
 from config import SETTINGS
@@ -16,8 +16,8 @@ logger = get_logger(__name__)
 logger.debug("In module %s", __name__)
 
 # connect to the duckdb database
-DUCKDB = connect(SETTINGS.duck_database)
-logger.info("Connected to duckdb database at %s", SETTINGS.duck_database)
+_DUCKDB = f"{SETTINGS.duck_database}market_holidays.parquet"
+logger.info("Using duckdb file at %s", _DUCKDB)
 
 def get_closed_count(start_date: date, end_date: date) -> int|None:
     """
@@ -28,29 +28,28 @@ def get_closed_count(start_date: date, end_date: date) -> int|None:
     :return: count of closed market holidays (int)
     """
     logger.debug("In function get_closed_count with start_date=%s, end_date=%s",
-                start_date, end_date)
+               start_date, end_date)
 
     # SQL query to get the count of closed market holidays
-    query = (
-        "SELECT COUNT(*) AS holiday_count "
-        "FROM market_holidays "
-        "WHERE Status = 'Closed' "
-        "AND Date >= ? "
-        "AND Date <= ?;"
+    result = sql(
+        f"SELECT COUNT(*) AS holiday_count "
+        f"FROM '{_DUCKDB}' "
+        f"WHERE Status = 'Closed' "
+        f"AND Date >= '{start_date}' "
+        f"AND Date <= '{end_date}';"
         )
-    # execute the query
-    result = DUCKDB.execute(query, (start_date, end_date)).fetchone()
     logger.debug("Query executed, result: %s", result)
 
     # extract the count from the result
-    if result is not None:
-        result = result[0]
-    else:
+    holiday_count = result.fetchone()
+    if holiday_count is None:
         raise LookupError("Could not retrieve closed market holiday count")
+    else:
+        holiday_count = holiday_count[0]
 
     # return the result
-    logger.debug("Exiting function get_closed_count with result=%s", result)
-    return result
+    logger.debug("Exiting function get_closed_count with result=%s", holiday_count)
+    return holiday_count
 
 def market_is_open(check_date: date) -> bool:
     """
@@ -67,28 +66,37 @@ def market_is_open(check_date: date) -> bool:
         return False
 
     # SQL query to check if the date is a market holiday
-    query = (
-        "SELECT Status "
-        "FROM market_holidays "
-        "WHERE Date = ?;"
+    result = sql(
+        f"SELECT Status "
+        f"FROM '{_DUCKDB}' "
+        f"WHERE Date = '{check_date}';"
         )
-    # execute the query
-    result = DUCKDB.execute(query, (check_date,)).fetchone()
     logger.debug("Query executed, result: %s", result)
 
     # determine if market is open
-    if result is None:
+    market_status = result.fetchone()
+    if market_status is None:
         # no entry means market is open
-        is_open = True
+        return_status = True
     else:
-        status = result[0]
-        is_open = status != 'Closed'
+        status = market_status[0]
+        return_status = status != 'Closed'
 
-    logger.debug("Exiting function market_is_open with is_open=%s", is_open)
-    return is_open
+    logger.debug("Exiting function market_is_open with is_open=%s", return_status)
+    return return_status
 
 if __name__ == "__main__":
     # test the get_closed_count function
-    # python -m utility.market_calendar
+    # RUN: python -m utility.market_calendar
     closed_count = get_closed_count(date(2024, 12, 31), date(2025, 8, 1))
     print(f"Closed market holidays between 2024-12-31 and 2025-08-01: {closed_count}")
+    closed_count = get_closed_count(date(2024, 10, 31), date(2024, 11, 1))
+    print(f"Closed market holidays between 2024-10-31 and 2024-11-01: {closed_count}")
+    closed_count = get_closed_count(date(2024, 11, 1), date(2024, 11, 1))
+    print(f"Closed market holidays between 2024-11-01 and 2024-10-31: {closed_count}")
+    test_date=date(2025,12,25)
+    is_open = market_is_open(test_date)
+    print(f"Market is open on {test_date}: {is_open}")
+    test_date=date(2025,7,24)
+    is_open = market_is_open(test_date)
+    print(f"Market is open on {test_date}: {is_open}")
