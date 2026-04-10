21ea91c1917bb8800d2de4ece2870bb2777baeda
Thomas Damon <nomadmot@mac.com>
2026-04-10 13:49:28 -0700

    split enumerations from Pydantic classes, export all classes from __init__.py

--- src/schemas/portfolio.py
+++ src/schemas/portfolio.py
@@ -1,66 +1,16 @@
 """
 This module defines Pydantic data models for the portfolio management system
 """
+
+# standard imports
 import datetime as dt
 from typing import Optional
+
+# 3rd party imports
 from pydantic import BaseModel, Field, field_validator
 
-# enumeration class for trade types
-class TradeType:
-    """
-    Class representing the different types of trades that
-    can be tracked in the portfolio management system.
-    """
-    BUY = "BUY"
-    SELL = "SELL"
-    TRANSFER = "TRAN"
-    EXERCISE = "EXRC"
-    EXPIRE = "EXPR"
-    ASSIGN = "ASGN"
-
-    Types = (BUY, SELL, TRANSFER, EXERCISE, EXPIRE, ASSIGN)
-
-    @classmethod
-    def is_valid(cls, value):
-        """
-        Validate the input value by verifying whether or not the value exists
-        within the TradeType enumeration
-
-        Arguments:
-            value -- The input value to be validated
-
-        Returns:
-            True if the input is valid, otherwise false
-        """
-        return value in (cls.Types)
-
-# enumeration class for security types
-class SecurityType:
-    """
-    Class representing the different types of securities that
-    can be tracked in the portfolio management system.
-    """
-    STOCK = 'S'
-    BOND = 'B'
-    ETF = 'E'
-    MUTUAL_FUND = 'M'
-    OPTION = 'O'
-
-    Types = (STOCK, BOND, ETF, MUTUAL_FUND, OPTION)
-
-    @classmethod
-    def is_valid(cls, value):
-        """
-        Validate the input value by verifying whether or not the value exists
-        within the SecurityType enumeration
-
-        Arguments:
-            value -- The input value to be validated
-
-        Returns:
-            True if the input is valid, otherwise false
-        """
-        return value in (cls.Types)
+# local imports
+from .enums import TradeType, SecurityType
 
 # --- Security Schema ---
 class Security(BaseModel):
@@ -77,6 +27,18 @@ class Security(BaseModel):
     @field_validator('security_type')
     @classmethod
     def validate_security_type(cls, v):
+        """
+        Validate the input security type against the SecurityType enumeration
+
+        Arguments:
+            v -- the input value
+
+        Raises:
+            ValueError: The input value failed verification
+
+        Returns:
+            The input Value
+        """
         if not SecurityType.is_valid(v):
             raise ValueError(f"Invalid security type: {v}. Must be one of {SecurityType.Types}")
         return v
@@ -107,6 +69,18 @@ class Trade(BaseModel):
     @field_validator('trade_type')
     @classmethod
     def validate_trade_type(cls, v):
+        """
+        Validate the input security type against the TradeType enumeration
+
+        Arguments:
+            v -- the input value
+
+        Raises:
+            ValueError: The input value failed verification
+
+        Returns:
+            The input Value
+        """
         if not TradeType.is_valid(v):
             raise ValueError(f"Invalid trade type: {v}. Must be one of {TradeType.Types}")
         return v
