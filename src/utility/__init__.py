30dcfc584a6bb762fda979c55dd79af272c42675
Thomas Damon <nomadmot@mac.com>
2026-04-13 15:20:34 -0700

    use relative imports for __init__.py

--- src/utility/__init__.py
+++ src/utility/__init__.py
@@ -2,16 +2,16 @@
 Initialize the utility module
 """
 # Export all utility module components for easy access
-from utility.logging import get_logger
-from utility.database import DATABASE_CONNECTION
-from utility.market_calendar import (
+from .logging import get_logger
+from .database import DATABASE_CONNECTION
+from .market_calendar import (
     get_closed_count,
     market_is_open,
 )
-from utility.status_message_component import get_status_message_component, StatusType
-from utility.autoupdate_multiselect_component import get_aumc_instance
-from utility.time_machine_component import get_time_machine_component
-from utility.system_info import get_client_ip, get_memory_size, show_system_info
+from .status_message_component import get_status_message_component, StatusType
+from .autoupdate_multiselect_component import get_aumc_instance
+from .time_machine_component import get_time_machine_component
+from .system_info import get_client_ip, get_memory_size, show_system_info
 
 __all__ = [
     "get_logger",
