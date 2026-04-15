a5e444453d40d63f908293cb6d363dcf5fb4da4e
Thomas Damon <nomadmot@mac.com>
2026-04-15 15:15:04 -0700

    fix ducklake connection logic, refactor imports

--- src/utility/database.py
+++ src/utility/database.py
@@ -4,7 +4,7 @@ import duckdb
 
 # import local libraries
 from config import SETTINGS
-from utility import get_logger
+from . import get_logger
 
 # initialize loggr
 _logger = get_logger(__name__)
@@ -26,19 +26,24 @@ def _database_engine():
                          "DUCK_PUDDLE (Data Lake Path) must be configured.")
 
     # connect to the Catalog Database
-    try:
+    # try:
         # Connect to the catalog database first. This DB holds the metadata.
-        con = duckdb.connect(catalog_uri)
-        _logger.info("Successfully connected to the DuckLake Catalog Database.")
-    except Exception as e:
-        _logger.error("Failed to connect to the DuckLake Catalog:", exc_info=True)
-        raise RuntimeError(f"Failed to initialize the DuckLake Catalog: {e}") from e
+    #     con = duckdb.connect(catalog_uri)
+    #     _logger.info("Successfully connected to the DuckLake Catalog Database.")
+    # except Exception as e:
+    #     _logger.error("Failed to connect to the DuckLake Catalog:", exc_info=True)
+    #     raise RuntimeError(f"Failed to initialize the DuckLake Catalog: {e}") from e
 
     # attach the Data Lake
     try:
         # This command tells DuckDB where the raw data resides.
         # We attach the data lake path as a virtual table source.
-        con.execute(f"ATTACH '{duck_puddle_path}' AS data_lake;")
+        sql_attach = f"""
+                    LOAD ducklake;
+                    ATTACH '{catalog_uri}' AS ducklake (DATA_PATH '{duck_puddle_path}');
+                    USE ducklake;
+                    """
+        con = duckdb.execute(sql_attach)
         _logger.info("Successfully attached the Data Lake at path: %s", duck_puddle_path)
 
         return con
