603bb362c6b4afbd8d2dd716a051f32d4d01c007
nomadmot <nomadmot@mac.com>
2026-08-22 15:17:14 -0600

    use prod database insead of test database

--- docker/run.sh
+++ docker/run.sh
@@ -8,7 +8,7 @@ docker container rm portfolio-dashboard
 
 # run the docker container, mounting the open_webui data valume
 docker run -d \
-    --volume /Users/nomadmot/Library/CloudStorage/Dropbox/Apps/Investing/DUCK_PUDDLE_TEST:/var/data\
+    --volume /Users/nomadmot/Library/CloudStorage/Dropbox/Apps/Investing/DUCK_PUDDLE:/var/data\
     --volume /Users/nomadmot/Library/CloudStorage/Dropbox/Apps/Investing/WATCHLISTS:/var/watchlists\
     --name portfolio-dashboard \
     --publish 8080:8501 \
